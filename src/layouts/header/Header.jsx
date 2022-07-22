03942d21b7a079636d2bdf18dc2bcf49f58374c5
ahmed03214 <109301629+ahmed03214@users.noreply.github.com>
2022-07-22 18:13:40 +0200

    Update Header.jsx

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -85,11 +85,13 @@ const Header = ({ showMobmenu, adminData }) => {
           </NavItem>
         </Nav>
         <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-          <DropdownToggle color="primary" className="px-0 px-md-3">
+          <DropdownToggle color="primary">
             <div
               className="d-flex justify-conent-cnter align-items-center gap-2"
               style={{ lineHeight: "0px" }}
             >
+              <p className="m-0">@{adminData.name}</p>
+
               <Image
                 src={mainAvatar}
                 alt="profile"
@@ -97,8 +99,6 @@ const Header = ({ showMobmenu, adminData }) => {
                 width="30"
                 height="30"
               />
-
-              <p className="m-0">@{adminData.name}</p>
             </div>
           </DropdownToggle>
           <DropdownMenu>
